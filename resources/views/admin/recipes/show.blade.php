97fbc56c69c7b02d6550558d4ebd128812127944
fedtoscano <federicotoscano611@gmail.com>
2024-08-19 20:22:20 +0200

    aggiunto il prezzo in euro alla show

--- resources/views/admin/recipes/show.blade.php
+++ resources/views/admin/recipes/show.blade.php
@@ -10,6 +10,7 @@
                 <div class="card-body">
                     <h5 class="card-title">{{$singleRecipe->name}}</h5>
                     <h5 class="card-title"><em>{{$singleRecipe->kitchen->name}} cuisine</em></h5>
+                    <h5 class="card-title">{{$singleRecipe->price}} EUR</h5>
                     @if ($singleRecipe->is_vegan==true)
                         <span class="badge rounded-pill text-bg-success">vegan</span>
                     @endif
